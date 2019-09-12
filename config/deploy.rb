cf164f44baab498134090c1da30d1f0ec97701e5
Jason Corum <jason.corum@gmail.com>
2019-09-12 13:58:31 -0700

    adds deploy from branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,13 @@ set :repo_url, 'https://github.com/WGBH/transcript-editor.git'
 set :rails_env, 'production'
 set :rvm_ruby_version, '2.3.0'
 
+# If the branch is not set with an env var, then ask for it.
+if ENV['AAPB_BRANCH']
+  set :branch, ENV['AAPB_BRANCH']
+else
+  ask :branch, 'master'
+end
+
 set :passenger_restart_command, 'rvmsudo /usr/sbin/apachectl restart'
 
 # Require confirmation by user if the repo is in a dirty state.
